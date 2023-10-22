d6df6f6b75761d4c35847d714598da3a7219b70d
mcventur <marcelinopenide@gmail.com>
2023-10-22 16:22:37 +0200

    Cierra teclado al pulsar Enter

--- app/src/main/java/com/mpd/pmdm/tiptimeviews/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/tiptimeviews/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mpd.pmdm.tiptimeviews
 
+import android.content.Context
 import android.icu.text.NumberFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.View.OnKeyListener
+import android.view.inputmethod.InputMethodManager
 import com.mpd.pmdm.tiptimeviews.databinding.ActivityMainBinding
 import kotlin.math.ceil
 
@@ -17,8 +21,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Empezamos a asociar eventos. Ojo a la forma de la llamada a setOnClickListener, con llaves
+        //Empezamos a asociar eventos. Ojo a la forma de la llamada a setOnClickListener con llaves: trailing lambda
         binding.calculateButton.setOnClickListener{calculateTip()}
+
+//        implementacion anonima de la interface OnKeyList
+//        val onKeyListener = object : OnKeyListener{
+//            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+
+//            }
+//        }
+
+        binding.costOfServiceEditText.setOnKeyListener{
+            //El tercer parámetro (event : KeyEvent?) no nos interesa. Le da _ como convención
+            view:View, keyCode: Int, _ -> manejaEventoTeclado(view, keyCode)
+        }
+
+
+
+    }
+
+    /**
+     * Ocultamos el teclado del View llamante si la tecla pulsada es Enter
+     */
+    private fun manejaEventoTeclado(view: View, keyCode: Int):Boolean {
+        if(keyCode == KeyEvent.KEYCODE_ENTER){
+            ocultarTeclado(view)
+            return true
+        }
+        return false
+    }
+
+    private fun ocultarTeclado(view: View) {
+        //apuntamos con una variable al servicio de manejo del teclado
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun calculateTip() {
