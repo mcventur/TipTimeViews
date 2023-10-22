66e01bcfe6ee549828f69e07416ac16ffb76db12
mcventur <marcelinopenide@gmail.com>
2023-10-22 15:46:11 +0200

    Nuevos test y Refactoring

--- app/src/androidTest/java/com/mpd/pmdm/tiptimeviews/CalculatorClassTest.kt
+++ app/src/androidTest/java/com/mpd/pmdm/tiptimeviews/CalculatorClassTest.kt
@@ -1,6 +1,5 @@
 package com.mpd.pmdm.tiptimeviews
 
-import android.icu.text.NumberFormat
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
@@ -28,42 +27,54 @@ class CalculatorClassTest {
 
     @Test
     fun calculate_20_percent_tip(){
+        typeCostOfService("50")
+
+        clickCalculateAndCheckResult(10)
+    }
+
+    @Test
+    fun calculate_20_percent_round_up(){
+        typeCostOfService("32")
+
+        //Activamos el switch de redondeo con un click
+        onView(withId(R.id.round_up_switch)).perform(click())
+
+        clickCalculateAndCheckResult(7)
+    }
+
+
+    @Test
+    fun calculate_15_percent_tip(){
+        typeCostOfService("100");
+
+        val interactionFifteenOption =  onView(withId(R.id.option_fifteen_percent))
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+        clickCalculateAndCheckResult(15)
+
+    }
+
+    /**
+     * Introduce el texto en el View cost_of_service_edit_text
+     */
+    private fun typeCostOfService(costOfService: String) {
         //Usamos funciones de la librería Espresso para interactuar con la IU desde código
 
         //Seleccionamos un elemento de la interfaz (el edit del coste del servicio), y le
         //escribimos una cantidad: 50
         onView(withId(R.id.cost_of_service_edit_text))
-            .perform(typeText("50"))
+            .perform(typeText(costOfService))
             .perform(ViewActions.closeSoftKeyboard())
-
-        //Lanzamos el cálculo haciendo click en el botón
-        onView(withId(R.id.calculate_button)).perform(click())
-
-        //Comprobamos el importe de la propina resultante en el TextView
-        //La función check recibe un ViewsAssertion como parámetro. Lo que queremos testear
-
-        //Calculamos el valor correcto, el esperado, igual que se hace en el código a testear
-        val expectedTip = java.text.NumberFormat.getCurrencyInstance().format(10)
-        val expectedTipText ="Tip Amount: " + expectedTip
-        onView(withId(R.id.tip_result)).check(matches(withText(expectedTipText)))
     }
 
-    @Test
-    fun calculate_20_percent_round_up(){
-        onView(withId(R.id.cost_of_service_edit_text))
-            .perform(typeText("32"))
-            .perform(ViewActions.closeSoftKeyboard())
-
-        //Activamos el switch de redondeo con un click
-        onView(withId(R.id.round_up_switch)).perform(click())
-
+    /**
+     * Pulsa el botón y chequea el resultado esperado
+     */
+    private fun clickCalculateAndCheckResult(value:Int) {
         onView(withId(R.id.calculate_button)).perform(click())
 
-        val expectedTip = java.text.NumberFormat.getCurrencyInstance().format(7)
-        val expectedTipText ="Tip Amount: " + expectedTip
+        val expectedTip = java.text.NumberFormat.getCurrencyInstance().format(value)
+        val expectedTipText = "Tip Amount: " + expectedTip
         onView(withId(R.id.tip_result)).check(matches(withText(expectedTipText)))
-
-
     }
 
 
