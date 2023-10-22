949f21c15e20f21ae0a26977c97e3cd799fa8b8b
mcventur <marcelinopenide@gmail.com>
2023-10-22 13:34:32 +0200

    Test OK

--- app/src/androidTest/java/com/mpd/pmdm/tiptimeviews/CalculatorClassTest.kt
+++ app/src/androidTest/java/com/mpd/pmdm/tiptimeviews/CalculatorClassTest.kt
@@ -2,6 +2,7 @@ package com.mpd.pmdm.tiptimeviews
 
 import android.icu.text.NumberFormat
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -31,15 +32,20 @@ class CalculatorClassTest {
 
         //Seleccionamos un elemento de la interfaz (el edit del coste del servicio), y le
         //escribimos una cantidad: 50
-        onView(withId(R.id.cost_of_service_edit_text)).perform(
-            typeText("50")
-        )
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50"))
+            .perform(ViewActions.closeSoftKeyboard())
+
         //Lanzamos el cálculo haciendo click en el botón
         onView(withId(R.id.calculate_button)).perform(click())
 
         //Comprobamos el importe de la propina resultante en el TextView
         //La función check recibe un ViewsAssertion como parámetro. Lo que queremos testear
-        onView(withId(R.id.tip_result)).check(matches(withText(NumberFormat.getCurrencyInstance().format("10.00"))))
+
+        //Calculamos el valor correcto, el esperado, igual que se hace en el código a testear
+        val expectedTip = java.text.NumberFormat.getCurrencyInstance().format(10)
+        val expectedTipText ="Tip Amount: " + expectedTip
+        onView(withId(R.id.tip_result)).check(matches(withText(expectedTipText)))
     }
 
 
